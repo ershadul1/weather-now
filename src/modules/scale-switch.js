d9177985763d59a73fdfe04381acf5f7347f9bba
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-28 20:49:34 +0600

    Add radio buttons to switch temp scale

---
+++ src/modules/scale-switch.js
@@ -0,0 +1,38 @@
+import populateData from './populate-data';
+
+const scaleSwitcher = () => {
+  const navContainer = document.querySelector('#nav-container');
+  const switchDiv = document.createElement('div');
+  switchDiv.classList.add('switch-field');
+
+  const celciusBtn = document.createElement('input');
+  celciusBtn.setAttribute('type', 'radio');
+  celciusBtn.setAttribute('id', 'radio-one');
+  celciusBtn.setAttribute('name', 'switch-one');
+  celciusBtn.setAttribute('value', 'C');
+  celciusBtn.checked = true;
+  celciusBtn.onclick = () => {
+    populateData();
+  };
+  const celciusBtnLabel = document.createElement('label');
+  celciusBtnLabel.setAttribute('for', 'radio-one');
+  celciusBtnLabel.textContent = 'Celcius';
+
+  const farenheitBtn = document.createElement('input');
+  farenheitBtn.setAttribute('type', 'radio');
+  farenheitBtn.setAttribute('id', 'radio-two');
+  farenheitBtn.setAttribute('name', 'switch-one');
+  farenheitBtn.setAttribute('value', 'F');
+  farenheitBtn.onclick = () => {
+    populateData();
+  };
+  const farenheitBtnLabel = document.createElement('label');
+  farenheitBtnLabel.setAttribute('for', 'radio-two');
+  farenheitBtnLabel.textContent = 'Farenheit';
+
+
+  switchDiv.append(celciusBtn, celciusBtnLabel, farenheitBtn, farenheitBtnLabel);
+  navContainer.appendChild(switchDiv);
+};
+
+export default scaleSwitcher;
