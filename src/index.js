77cc7adc129f1ffaedbd6e1c5c57c6d3741b5af3
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-28 20:50:47 +0600

    Add more containers in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import form from './modules/form';
+import scaleSwitcher from './modules/scale-switch';
 import './styles/style.css';
+import getWeatherData from './modules/get-weather-data';
+
+
+const container = document.querySelector('#container');
+const navContainer = document.createElement('div');
+navContainer.setAttribute('id', 'nav-container');
+const weatherDataContainer = document.createElement('div');
+weatherDataContainer.setAttribute('id', 'weather-data-container');
+container.append(navContainer, weatherDataContainer);
+
+form();
+scaleSwitcher();
+getWeatherData('dhaka');
